6513b9eaadd146b4a41eced3dd494beed9745315
CharlesChinedum <charleschinedum2@gmail.com>
2024-02-28 00:49:03 +0100

    update: Implemented UI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import logo from "../logo.svg"
+import "./index.css"
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +10,25 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const currentPath = window.location.href;
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <div className="">
+        <img src={logo} alt="logo" className='logo' />
+      </div>
+      <div className="container">
+        <div className="inner-container">
+          <h2 className="header">
+            You’ve just ignited a new <span className="library">Remix</span> static site!
+          </h2>
+          <span className="pathname">
+            {currentPath}
+          </span>
+        </div>
+        <p className="footer">
+          Made in Nigeria with 💖
+        </p>
+      </div>
+    </main>
   );
 }
